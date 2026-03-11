7e29123f6ecab2b210e42a429fb283d8753f2288
Olofin Deborah <deboraholofin43@gmail.com>
2026-03-11 19:23:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ page = st.sidebar.selectbox(
 )
 
 # -- CREATING PAGE 1 AND PAGE 2 --
+# page 1
 if page == "Model Comparison":
   st.header("Model Comparison")
   st.subheader("All Model Results")
@@ -26,21 +27,68 @@ if page == "Model Comparison":
   st.write("The table below shows the wnning forecasting model for each state and commodity combination, selected based on lowest average MAPE across all three forecast horizons.")
   st.dataframe(best_models)
 
+#page 2
 elif page == "Forecast Explorer":
   st.header("Forecast Explorer")
+  # dropdown with state options
   state = st.selectbox(
     "Select a State",
     ["Adamawa", "Borno", Yobe]
   )
 
+  # dropdown with commodity options
   commodity = st.selectbox(
     "Select a Commodity",
     ["Rice (imported)", "Rice (local)", 
      "Beans (white)", "Yam", "Tomatoes"]
   )
 
+  #filters the model to get the dataset that th uer prompted
   filtered_best = best_models[
     (best_models['state'] == state) & (best_models['commodity'] == commodity) 
   ]
+
+  if len(filtered_best) == 0:
+    st.warning("No results found for this combination.")
+  else:
+    st.success(f"Best model for {commodity} in {state}: {filtered_best['model'}.values[0]}")
+    best_model_name = filtered_best['model'].valeus[0]
+
+    filtered_metrics = model_comparison[
+      (model_comparison['state'] == state) & 
+      (model_comparison['commodity'] == commodity) &
+      (model_comparison['best_model'] ==  best_model_name)
+    ]
+  
+    st.subheader("Model Performance Metrics")
+    st.dataframe(filtered_metrics[['model', 'MAPE', 'MAE', 'RMSE', 'H1_MAPE', 'H2_MAPE', 'H3_MAPE']])
+
+    st.subheader("Predicted Prices - 3 Months Ahead")
+
+    if best_model_name == "Prophet":
+      price_data = prophet_horizons[
+        (prophet_horizons['state'] == state) &
+        (prophet_horizons['commodity'] == commodity)
+      ]
+    elif best_model_name == "XGBoost":
+      price_data = xgb_horizons[
+        (xgb_horizons['state'] == state) &
+        (xgb_horizons['commodity'] == commodity)
+      ]
+    elif best_model_name == "SARIMA":
+      price_data = prophet_horizons[
+        (prophet_horizons['state'] == state) &
+        (prophet_horizons['commodity'] == commodity)
+      ]
+      st.info("Note: SARIMA does not support 3-month forecasts. Showing Prophet predictions for this combination.")
+    
+
+    st.dataframe(price_data[['date', 'horizon', 'actual', 'predicted', 'error_pct']])
+
+
+
+    
+    
+    
 
    
